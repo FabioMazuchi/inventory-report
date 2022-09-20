d413513b25e9334f40da238c6e03e66446f056de
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-20 15:43:00 -0300

    requisito 02

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,27 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    produto = "refrigerante"
+    empresa = "coca-cola"
+    fabricacao = "01-02-2022"
+    validade = "01-02-2023"
+    serie = "123"
+    instrucoes = "em ambiente refrigerado"
+
+    re = f"O produto {produto} fabricado em {fabricacao}"
+    sul = f"por {empresa} com validade até {validade} precisa"
+    result = f"{re} {sul} ser armazenado {instrucoes}."
+    product = str(
+        Product(
+            id,
+            produto,
+            empresa,
+            fabricacao,
+            validade,
+            serie,
+            instrucoes,
+        )
+    )
+    assert result == product
