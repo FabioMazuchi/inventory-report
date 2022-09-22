dfee282aa4f58eabc83e3576c9482fddc7729bfe
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-22 09:01:25 -0300

    requisito 09

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,19 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importer.csv_importer import CsvImporter
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    d1 = "\033[36m2020-09-06\033[0m\n"
+    d2 = "\033[36m2023-09-17\033[0m\n"
+    empr = "\033[31mTarget Corporation\033[0m"
+    fr1 = "\033[32mData de fabricação mais antiga:\033[0m " + d1
+    fr2 = "\033[32mData de validade mais próxima:\033[0m " + d2
+    fr3 = "\033[32mEmpresa com mais produtos:\033[0m " + empr
+
+    string = fr1 + fr2 + fr3
+    result = ColoredReport(SimpleReport).generate(
+        CsvImporter.import_data("inventory_report/data/inventory.csv")
+    )
+
+    assert string == result
